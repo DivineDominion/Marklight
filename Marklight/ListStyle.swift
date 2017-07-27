f123d36e1d018153c4e99169ccf12d6468a3385c
Christian Tietze <christian.tietze@gmail.com>
2017-07-27 16:29:02 +0200

    match only first line's occurence of list markers

--- Marklight/ListStyle.swift
+++ Marklight/ListStyle.swift
@@ -13,22 +13,23 @@ struct ListStyle: BlockStyle {
     fileprivate static var _markerUL: String { return "[*+-]" }
     fileprivate static var _markerOL: String { return "\\d+[.]" }
     fileprivate static var _listMarker: String { return "(?:\(_markerUL)|\(_markerOL))" }
-    fileprivate static let listOpeningRegex = Regex(pattern: _listMarker, options: [.allowCommentsAndWhitespace])
+    fileprivate static let listOpeningRegex = Regex(pattern: "^\\p{Z}*\(_listMarker)", options: [.anchorsMatchLines])
 
-    fileprivate static var _wholeList: String { return [
-        "(                               # $1 = whole list",
-        "  (                             # $2",
+    fileprivate static var listPattern: String { return [
+        "(?:(?<=\\n\\n)|\\A\\n?)",
+        "(",                     // $1 = whole list
+        "  (",                   // $2
         "    \\p{Z}{0,3}",
-        "    (\(_listMarker))            # $3 = first list item marker",
+        "    (\(_listMarker))",  // $3 = first list item marker
         "    \\p{Z}+",
         "  )",
         "  (?s:.+?)",
-        "  (                             # $4",
+        "  (",                   // $4
         "      \\z",
         "    |",
         "      \\n{2,}",
         "      (?=\\S)",
-        "      (?!                       # Negative lookahead for another list item marker",
+        "      (?!",             // Negative lookahead for another list item marker
         "        \\p{Z}*",
         "        \(_listMarker)\\p{Z}+",
         "      )",
@@ -36,7 +37,7 @@ struct ListStyle: BlockStyle {
         ")"
         ].joined(separator: "\n")
     }
-    fileprivate static var listPattern: String { return "(?:(?<=\\n\\n)|\\A\\n?)" + _wholeList }
+
     fileprivate static let listRegex = Regex(pattern: listPattern, options: [.allowCommentsAndWhitespace, .anchorsMatchLines])
 
     func apply(_ theme: MarklightTheme, styleApplier: MarklightStyleApplier, hideSyntax: Bool, document: Document) {

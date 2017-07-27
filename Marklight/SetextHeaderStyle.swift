39107fd094b40e2015e3aca53476e05fb092e2d8
Christian Tietze <christian.tietze@gmail.com>
2017-07-27 10:10:38 +0200

    fix Setext header colors being switched

--- Marklight/SetextHeaderStyle.swift
+++ Marklight/SetextHeaderStyle.swift
@@ -18,10 +18,10 @@ import Foundation
 struct SetextHeadingStyle: BlockStyle {
 
     fileprivate static var headerSetextPattern: String { return [
-        "^(.+?)",
+        "^(.+?)",     // $1 = Heading text
         "\\p{Z}*",
         "\\n",
-        "(==+|--+)",  // $1 = string of ='s or -'s
+        "(==+|--+)",  // $2 = string of ='s or -'s
         "\\p{Z}*",
         "\\n+"
         ].joined(separator: "\n")
@@ -32,7 +32,7 @@ struct SetextHeadingStyle: BlockStyle {
     func apply(_ theme: MarklightTheme, styleApplier: MarklightStyleApplier, hideSyntax: Bool, document: Document) {
         SetextHeadingStyle.headersSetextRegex.matches(document) { (result) -> Void in
             styleApplier.embolden(range: result.range)
-            theme.syntaxStyle.apply(styleApplier, range: result.rangeAt(1))
+            theme.syntaxStyle.apply(styleApplier, range: result.rangeAt(2))
         }
     }
 }
